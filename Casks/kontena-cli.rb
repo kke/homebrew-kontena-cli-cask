fbd210e3e8b0fae79a23e4d7a7f8ec2fa07818d4
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-02-09 11:48:06 +0200

    versioning

--- Casks/kontena-cli.rb
+++ Casks/kontena-cli.rb
@@ -1,6 +1,7 @@
 cask 'kontena-cli' do
   appcast 'https://github.com/kontena/kontena/releases.atom',
           checkpoint: '85e8d0fc3b07695f9825ed57b6bf3269ea0ec624247fcc453a343e183395f332'
+  version :latest
   url "https://github.com/kontena/kontena/releases/download/#{version}/kontena-cli-osx-#{version}-amd64.pkg"
   homepage 'https://kontena.io/'
   pkg "kontena-cli-osx-#{version}-amd64.pkg"
