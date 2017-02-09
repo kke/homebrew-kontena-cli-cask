1f765ed94c9e23191a01b1dbbc4e7563451397e3
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-02-09 11:15:21 +0200

    checkpoint

--- Casks/kontena-cli.rb
+++ Casks/kontena-cli.rb
@@ -1,5 +1,6 @@
 class KontenaCli < Cask
-  appcast 'https://github.com/kontena/kontena/releases.atom'
+  appcast 'https://github.com/kontena/kontena/releases.atom',
+          checkpoint: '85e8d0fc3b07695f9825ed57b6bf3269ea0ec624247fcc453a343e183395f332'
   url "https://github.com/kontena/kontena/releases/download/#{version}/kontena-cli-osx-#{version}-amd64.pkg"
   homepage 'https://kontena.io/'
   pkg "kontena-cli-osx-#{version}-amd64.pkg"
