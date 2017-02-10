f2fdce7568110d2d869844f573b6e90d10b0f207
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-02-10 11:01:42 +0200

    more tweakery

--- Casks/kontena-cli.rb
+++ Casks/kontena-cli.rb
@@ -2,9 +2,10 @@ cask 'kontena-cli' do
   version '1.1.1'
   sha256 'a764f33f7b87ddbfcb844d09180956519c049b8b54c18967020741f17c39de96'
 
+  # URL_SECTION was verified as official when first introduced to the cask
   url "https://github.com/kontena/kontena/releases/download/v#{version}/kontena-cli-osx-v#{version}-amd64.pkg"
   appcast 'https://github.com/kontena/kontena/releases.atom',
-    checkpoint: '85e8d0fc3b07695f9825ed57b6bf3269ea0ec624247fcc453a343e183395f332'
+          checkpoint: '85e8d0fc3b07695f9825ed57b6bf3269ea0ec624247fcc453a343e183395f332'
   name 'kontena-cli'
   homepage 'https://kontena.io/'
 
@@ -12,4 +13,3 @@ cask 'kontena-cli' do
 
   uninstall pkgutil: 'io.kontena.pkg.cli'
 end
-
