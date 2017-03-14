a4f0d2f5792b5dfc65e175754f31b46d9beb0435
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-03-14 09:53:10 +0200

    V1.1.4

--- Casks/kontena-cli.rb
+++ Casks/kontena-cli.rb
@@ -1,11 +1,11 @@
 cask 'kontena-cli' do
-  version '1.1.1'
-  sha256 'a764f33f7b87ddbfcb844d09180956519c049b8b54c18967020741f17c39de96'
+  version '1.1.4'
+  sha256 '86fd732ee56f797df7944b535389a405c45086556ce573e941b4146a9f2924f2'
 
   # github.com was verified as official when first introduced to the cask
   url "https://github.com/kontena/kontena/releases/download/v#{version}/kontena-cli-osx-v#{version}-amd64.pkg"
   appcast 'https://github.com/kontena/kontena/releases.atom',
-          checkpoint: '85e8d0fc3b07695f9825ed57b6bf3269ea0ec624247fcc453a343e183395f332'
+          checkpoint: '9f25b29a996ad30c1df266aa31c9b096fd9a26d42f05d7500e553c47105fa48f'
   name 'kontena-cli'
   homepage 'https://kontena.io/'
 
